b48dac31390ec5761dc840d7ee8a96db3ba7574a
KhushiChauhan8 <kc6478576@gmail.com>
2026-01-31 21:37:11 +0530

    add login functionality

--- src/controllers/auth.controller.js
+++
@@ -1,9 +0,0 @@
-export const loginUser = (req, res) => {
-    const { email } = req.body;
-
-    res.status(200).json({
-        success: true,
-        message: 'Login API Working',
-        email,
-    });
-};
--- src/routes/fees.routes.js
+++ src/routes/fees.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { addFees, deleteFees, getAllFees, getFeesByStudentId, updateFeesStatus } from '../controllers/fees.controller.js';
 import { feesVslidation } from '../middlewares/validation.middleware.js';
+import { protect } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/add", feesVslidation, addFees);
-router.get("/all", getAllFees);
-router.get("/student/:studentId", getFeesByStudentId);
-router.put("/update/:id", updateFeesStatus);
-router.delete("/delete/:id", deleteFees);
+router.post("/add", protect, feesVslidation, addFees);
+router.get("/all", protect, getAllFees);
+router.get("/student/:studentId", protect, getFeesByStudentId);
+router.put("/update/:id", protect, updateFeesStatus);
+router.delete("/delete/:id", protect, deleteFees);
 
 export default router;
